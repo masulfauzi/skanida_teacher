77c3169a38f766613a210a8ffc48edac4b3d7970
Mas'ul <masul.fauzi@gmail.com>
2026-02-27 13:16:16 +0700

    feat: tambah icon louncher

--- lib/main.dart
+++ lib/main.dart
@@ -16,6 +16,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Skanida Teacher',
       theme: ThemeData(primarySwatch: Colors.deepPurple),
       home: const SplashScreen(),
